35f3feb86ed4fd65518905b4f961e99d8bc6074b
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-09-12 20:26:12 -0500

    Update routeBasePath in Docusaurus config to match project structure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
          /** @type {import('@docusaurus/preset-classic').Options} */
          ({
             docs: {
-               routeBasePath: '/',
+               routeBasePath: '/my-lovecppdsa-journey',
                sidebarPath: './sidebars.js',
             },
             theme: {
