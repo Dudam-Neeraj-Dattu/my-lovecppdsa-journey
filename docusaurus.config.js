198e23d5027da30c12efdce4af3f59076f2f4ea8
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-09-12 20:15:28 -0500

    Refactor GitHub Actions workflow for Docusaurus deployment and update configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
    // If you aren't using GitHub pages, you don't need these.
    organizationName: 'Dudam-Neeraj-Dattu', // Usually your GitHub org/user name.
    projectName: 'my-lovecppdsa-journey', // Usually your repo name.
-
+   trailingSlash : false,
    onBrokenLinks: 'throw',
    onBrokenMarkdownLinks: 'warn',
 
