72d36c4149af43c5b8ece8ddae0af7aa7627f4eb
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-09-12 20:28:19 -0500

    Update baseUrl and routeBasePath in Docusaurus config for GitHub Pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
    url: 'https://dudam-neeraj-dattu.github.io',
    // Set the /<baseUrl>/ pathname under which your site is served
    // For GitHub pages deployment, it is often '/<projectName>/'
-   baseUrl: '/my-lovecppdsa-journey/',
+   baseUrl: '/',
 
    // GitHub pages deployment config.
    // If you aren't using GitHub pages, you don't need these.
@@ -47,7 +47,7 @@ const config = {
          /** @type {import('@docusaurus/preset-classic').Options} */
          ({
             docs: {
-               routeBasePath: '/my-lovecppdsa-journey',
+               routeBasePath: '/',
                sidebarPath: './sidebars.js',
             },
             theme: {
