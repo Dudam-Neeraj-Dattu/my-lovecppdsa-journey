ac9a87ab5271b06371e172d6c6838fbf9a239714
Dudam Neeraj Dattu <dudamdattu@gmail.com>
2025-09-12 20:43:04 -0500

    Update baseUrl in Docusaurus config to correct path for documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
    url: 'https://dudam-neeraj-dattu.github.io',
    // Set the /<baseUrl>/ pathname under which your site is served
    // For GitHub pages deployment, it is often '/<projectName>/'
-   baseUrl: '/my-lovecppdsa-journey/docs/',
+   baseUrl: '/my-lovecppdsa-journey/',
 
    // GitHub pages deployment config.
    // If you aren't using GitHub pages, you don't need these.
